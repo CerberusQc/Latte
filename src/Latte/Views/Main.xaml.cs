73de9e6a675330825863cd111dda5035e6f8754d
Bastien Roy <bastien@edgegap.com>
2022-04-08 02:17:28 -0400

    Base Project

---
+++ src/Latte/Views/Main.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Latte.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Latte
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            SaveSettings();
+            base.OnClosing(e);
+        }
+
+        private void SaveSettings()
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                Settings.Default.Height = this.Height;
+                Settings.Default.Width = this.Width;
+            }
+            else
+            {
+                Settings.Default.Height = this.RestoreBounds.Height;
+                Settings.Default.Width = this.RestoreBounds.Width;
+            }
+
+            Settings.Default.Save();
+
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+    }
+}
